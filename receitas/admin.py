1d374b872afdfada71ca6307714cf121d840f557
Matheus Nascimento <mnsergio59@gmail.com>
2022-08-03 11:42:21 -0300

    ajustes

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Receita
 
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'publicada')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_editable = ('publicada',)
+
 # Register your models here.
-admin.site.register(Receita)
+admin.site.register(Receita, ListandoReceitas)
