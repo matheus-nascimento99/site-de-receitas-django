572861daca1b648026bf03323fa54a1f9013b3de
Matheus Nascimento <mnsergio59@gmail.com>
2022-08-03 15:13:33 -0300

    ajustes v2

--- receitas/views.py
+++ receitas/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import get_object_or_404, render, get_list_or_404
 from .models import Receita
 # Create your views here.
@@ -20,3 +21,17 @@ def receita(request, pk):
     }
     
     return render(request, 'receita.html', context)
+
+def buscar(request):
+    receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if (buscar):
+            receitas = receitas.filter(nome_receita__icontains=nome_a_buscar)
+            
+    dados = {
+        'receitas':receitas
+    }
+
+    return render(request, 'buscar.html', dados)
